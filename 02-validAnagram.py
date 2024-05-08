d0041a2c8daf46bfd2e5206a1fdcb25f84986f8a
Corina Conklin <130714271+CorinaNC@users.noreply.github.com>
2024-05-08 17:31:08 -0500

    Delete 02-validAnagram.py

--- 02-validAnagram.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(list(s)) == sorted(list(t))
-
-# compare every character in s to every character in t
-# create a list of characters for both and sort? if both are equal then true.
