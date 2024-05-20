87a8d84e6a6f2b26677d12362b8a21037f6cdadd
Corina <conkl095@umn.edu>
2024-05-20 18:25:24 -0500

    .

--- neetcode.io/10-isPalindrome.py
+++ neetcode.io/10-isPalindrome.py
@@ -1,8 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = (''.join(filter(str.isalnum, s))).lower()
-        print(s)
         return s == s[::-1]
+
 
 # basically just have to convert characters into
 # lowercase, remove whitespace and non alphabeticals
